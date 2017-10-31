892ca0d0af8fdc2d314dd6504bf2ac21c70e1449
Frankie Smith <fsmith1989@gmail.com>
2017-10-31 15:07:35 -0400

    Beautify

--- scripts.js
+++ scripts.js
@@ -1,17 +1,17 @@
-$(document).ready(function() {
-$('.button').on('click', function() {
-    $('.pop-up-container').animate({
-        'width': 'show'
-    }, 1000, function() {
-        $('.pop-up-container').fadeIn(500);
-    });
-});
-	
-$('.pop-up').on('click', function() {
-    $('.pop-up-container').fadeOut(500, function() {
-        $('.pop-up-container').animate({
-            'width': 'hide'
-        }, 1000);
-    });
-});
+$(document).ready(function () {
+	$('.button').on('click', function () {
+		$('.pop-up-container').animate({
+			'width': 'show'
+		}, 1000, function () {
+			$('.pop-up-container').fadeIn(500);
+		});
+	});
+
+	$('.pop-up').on('click', function () {
+		$('.pop-up-container').fadeOut(500, function () {
+			$('.pop-up-container').animate({
+				'width': 'hide'
+			}, 1000);
+		});
+	});
 });
